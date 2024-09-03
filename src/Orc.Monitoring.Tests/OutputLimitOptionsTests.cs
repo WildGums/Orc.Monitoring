a0d4f5f638ea245740c40820258e1217e73c1371
Maksim Khomutov <mkhomutov@gmail.com>
2024-09-03 10:03:46 +0800

    extend OutputLimitOptionsTests

--- src/Orc.Monitoring.Tests/OutputLimitOptionsTests.cs
+++ src/Orc.Monitoring.Tests/OutputLimitOptionsTests.cs
@@ -91,4 +91,74 @@ public class OutputLimitOptionsTests
 
         Assert.That(options1.GetHashCode(), Is.EqualTo(options2.GetHashCode()));
     }
+
+
+    [Test]
+    public void MaxItems_CanBeSetToMaxIntValue()
+    {
+        var options = new OutputLimitOptions { MaxItems = int.MaxValue };
+
+        Assert.That(options.MaxItems, Is.EqualTo(int.MaxValue));
+        Assert.That(options.HasLimits, Is.True);
+    }
+
+    [Test]
+    public void MaxItems_CanBeSetToOne()
+    {
+        var options = new OutputLimitOptions { MaxItems = 1 };
+
+        Assert.That(options.MaxItems, Is.EqualTo(1));
+        Assert.That(options.HasLimits, Is.True);
+    }
+
+    [Test]
+    public void LimitItems_WithZero_CreatesOptionsWithZeroMaxItems()
+    {
+        var options = OutputLimitOptions.LimitItems(0);
+
+        Assert.That(options.MaxItems, Is.EqualTo(0));
+        Assert.That(options.HasLimits, Is.True);
+    }
+
+    [Test]
+    public void LimitItems_WithMaxIntValue_CreatesOptionsWithMaxIntValueMaxItems()
+    {
+        var options = OutputLimitOptions.LimitItems(int.MaxValue);
+
+        Assert.That(options.MaxItems, Is.EqualTo(int.MaxValue));
+        Assert.That(options.HasLimits, Is.True);
+    }
+
+    [Test]
+    public void LimitItems_WithNegativeValue_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => OutputLimitOptions.LimitItems(-1));
+    }
+
+    [Test]
+    public void Equals_ReturnsTrueForBothUnlimitedOptions()
+    {
+        var options1 = OutputLimitOptions.Unlimited;
+        var options2 = new OutputLimitOptions();
+
+        Assert.That(options1, Is.EqualTo(options2));
+    }
+
+    [Test]
+    public void Equals_ReturnsFalseForUnlimitedAndLimitedOptions()
+    {
+        var unlimited = OutputLimitOptions.Unlimited;
+        var limited = OutputLimitOptions.LimitItems(100);
+
+        Assert.That(unlimited, Is.Not.EqualTo(limited));
+    }
+
+    [Test]
+    public void GetHashCode_ReturnsDifferentValuesForDifferentOptions()
+    {
+        var options1 = new OutputLimitOptions { MaxItems = 100 };
+        var options2 = new OutputLimitOptions { MaxItems = 200 };
+
+        Assert.That(options1.GetHashCode(), Is.Not.EqualTo(options2.GetHashCode()));
+    }
 }
