3c3463da60dd4a67ed363238660c015f376b60f7
Geert van Horrik <GeertvanHorrik@users.noreply.github.com>
2024-11-15 17:00:55 +0100

    GitHubSync update

--- src/MethodTimeLogger.cs
+++ src/MethodTimeLogger.cs
@@ -1,5 +1,5 @@
-﻿using System.Reflection;
-//using Catel.Logging;
+using System.Reflection;
+using Catel.Logging;
 using System;
 using System.Globalization;
 
@@ -33,7 +33,7 @@ internal static class MethodTimeLogger
             finalMessage += $" | {message}";
         }
 
-        //var logger = LogManager.GetLogger(type);
-        //logger.Debug(finalMessage);
+        var logger = LogManager.GetLogger(type);
+        logger.Debug(finalMessage);
     }
 }
